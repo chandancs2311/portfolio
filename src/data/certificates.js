5f340528c5be2fd36b36afab5fa9629dd2b351a9
chandan <chandancs2311@gmail.com>
2025-12-31 12:52:40 +0530

    changes

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,31 +1,31 @@
 const certificates = [
   {
-    title: "NPTEL Python",
-    file: "/src/assets/certificates/nptel/python.pdf",
+    title: "NPTEL Java",
+    file: "https://drive.google.com/file/d/1M1CRww23raK4_kPz-0BtenDGcLFx2xwB/view?usp=sharing",
   },
   {
-    title: "NPTEL Java",
-    file: "/src/assets/certificates/nptel/java.pdf",
+    title: "Intern Acmegrade",
+    file: "https://drive.google.com/file/d/1vGQ5djDhVuCK_34lf3bDGixUrOiNu4Jl/view?usp=sharing",
   },
   {
     title: "MongoDB Certification",
-    file: "/src/assets/certificates/mongodb/MongoDB chandan c s.pdf",
-  },
-  {
-    title: "Hackathon Participation",
-    file: "/src/assets/certificates/hackathon/Hackathon.pdf",
-  },
-  {
-    title: "Internship Certificate 1",
-    file: "/src/assets/certificates/internships/active-internship-certificate.pdf",
-  },
-  {
-    title: "Internship Certificate 2",
-    file: "/src/assets/certificates/internships/psiog.pdf",
+    file: "https://drive.google.com/file/d/135KCsZkbCgXRidUaMX299ZI0atNXUIvY/view?usp=sharing",
   },
   {
     title: "Conference Participation",
-    file: "/src/assets/certificates/conference/conference certificate.pdf",
+    file: "https://drive.google.com/file/d/1dTzRgBq1etP58tj5CwlR8E1fzqg5BYP1/view?usp=sharing",
+  },
+  {
+    title: "NPTEL Python",
+    file: "https://drive.google.com/file/d/1zOQoGyZoKyNA3ocTTYPdIUKFAsadkHq2/view?usp=sharing",
+  },
+  {
+    title: "Hackathon",
+    file: "https://drive.google.com/file/d/1SaDSonnEMdrqcyE8Wyc6e660hYYsVLug/view?usp=sharing",
+  },
+  {
+    title: "Intern Acmegrade",
+    file: "https://drive.google.com/file/d/1fxPs5vn1Be5RgM9hkgZ4lu99O6bhm7EE/view?usp=sharing",
   },
 ];
 
